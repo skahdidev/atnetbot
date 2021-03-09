0521b93f4190acd07e5d829717084177f6e4f8b5
SnepDev <56943387+skahdidev@users.noreply.github.com>
2021-03-09 13:47:00 +0200

    Update loop.py

--- monkeytelepot/loop.py
+++ monkeytelepot/loop.py
@@ -110,7 +110,8 @@ def _extract_message(update):
                                    'chosen_inline_result',
                                    'shipping_query',
                                    'pre_checkout_query',
-                                   'poll','kicked', 'status'])
+                                   'poll','kicked', 'status',
+                                   'my_chat_member'])
     return key, update[key]
 
 def _infer_handler_function(bot, h):
